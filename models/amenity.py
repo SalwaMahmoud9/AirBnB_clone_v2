254f72ef55a19742cd022c0182316a48d3ac79e4
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-03-18 14:39:03 +0200

    salwa 18-3-2024

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Amenity"""
 
+"""
+A module that defines the ORM class for Amenity table
+"""
 from os import getenv
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
 from models.base_model import Base, BaseModel
-import json
 
 
 class Amenity(BaseModel, Base):
-    """child from BaseModel class"""
-    
+    """
+    Defines Amenity class attributes
+    """
     __tablename__ = 'amenities'
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
